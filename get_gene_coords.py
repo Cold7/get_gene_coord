014d011f27aaf8b275d83fdcbdf57d4e9326106a
Cold7 <contrerasriquelme.sebastian@gmail.com>
2020-04-02 16:36:26 -0300

    Add files via upload

--- get_gene_coords.py
+++ get_gene_coords.py
@@ -1,20 +1,28 @@
-f = open("gene_coords.bed","r") #gene_coord.bed lo saque con bedops de esta forma (el awk es para evitar el error de missing transcript)
-#awk '{ if ($0 ~ "transcript_id") print $0; else print $0" transcript_id \"\";"; }' ENCFF159KBI.gtf | gtf2bed - | cut -f1-6> gene_coords.bed 
-data = {}
+#f = open("gene_coords.bed","r") #gene_coord.bed lo saque con bedops de esta forma (el awk es para evitar el error de missing transcript)
+##awk '{ if ($0 ~ "transcript_id") print $0; else print $0" transcript_id \"\";"; }' ENCFF159KBI.gtf | gtf2bed - | cut -f1-6> gene_coords.bed 
+#data = {}
+#for line in f:
+#	aux = line[:-1].split("\t")
+#	chr = aux[0]
+#	init = aux[1]
+#	end = aux[2]
+#	gene = aux[3]
+#	geneID = chr+"_"+gene
+#	strand = aux[5]
+#	if id not in data:
+#		data[geneID] = {"chr":chr, "gene":gene,"init": init,"end":end}
+#
+#	else:
+#		data[geneID]["end"] = end
+#	
+#
+#for gene in data:
+#	print(data[gene]["chr"]+"\t"+data[gene]["gene"]+"\t"+data[gene]["init"]+"\t"+data[gene]["end"])
+
+
+f = open("ENCFF159KBI.gtf","r")
 for line in f:
 	aux = line[:-1].split("\t")
-	chr = aux[0]
-	init = aux[1]
-	end = aux[2]
-	gene = aux[3].split(".")[0]
-	geneID = chr+"_"+gene
-	strand = aux[5]
-	if id not in data:
-		data[geneID] = {"chr":chr, "gene":gene,"init": init,"end":end}
-
-	else:
-		data[geneID]["end"] = end
-	
-
-for gene in data:
-	print(data[gene]["chr"]+"\t"+data[gene]["gene"]+"\t"+data[gene]["init"]+"\t"+data[gene]["end"])
+	if aux[2] == "gene":
+		geneID = line.split("gene_id \"")[1].split("\"")[0]
+		print(geneID+"\t"+aux[0]+"\t"+aux[3]+"\t"+aux[4])
